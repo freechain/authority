588435df433146e3b8f99d0fbc64f5b3915b44d9
Andrey <a.stegno@gmail.com>
2018-03-17 02:13:02 +0000

    improved contracts

--- blockchain/solidity/NameRegistry.sol
+++ blockchain/solidity/NameRegistry.sol
@@ -11,13 +11,26 @@ contract NameRegistry is SafeMath, Ownable {
     
     mapping (address => uint256) registrants;
     
+    mapping (address => address) sales;
+    
     function() public payable {
     
-        require(safeSub(available, msg.value)  >= 0 || registrants[msg.sender] > 0);
+        uint256 rest = safeSub(available, msg.value);
+    
+        require(rest  >= 0 || registrants[msg.sender] > 0);
         
-        available = safeSub(available, msg.value);
+        if (rest >= 0) {
+            
+            available = rest;
+        } 
+        
+        sales[msg.sender] = msg.sender;
         
         registrants[msg.sender] = safeAdd(registrants[msg.sender], msg.value);
+    }
+    
+    function assignSales(address _sales) public {
+        sales[msg.sender] = _sales;  
     }
 
     mapping (bytes32 => address) public registry;
@@ -27,19 +40,35 @@ contract NameRegistry is SafeMath, Ownable {
     function transferName(bytes32 _name, address _to) public {
         
         require(registryOwners[_name] == msg.sender);
+        
         registryOwners[_name] = _to;
     }
     
+    function changeAddress(bytes32 _name, address _to) public {
+    
+        require(registryOwners[_name] == msg.sender);
+        
+        registry[_name] = _to;
+    }
+    
+    
     function registerName(
+        address _registrant,
         bytes32 _name,
         address _address
     ) public {
+
+        require(sales[_registrant] == msg.sender);
         
         require(registry[_name] == 0x0);
-        require(registrants[msg.sender] >= price);
-        registrants[msg.sender] = safeSub(registrants[msg.sender], price);
+        
+        require(registrants[_registrant] >= price);
+        
+        registrants[_registrant] = safeSub(registrants[_registrant], price);
+        
         registry[_name] = _address;
-        registryOwners[_name] = msg.sender;
+        
+        registryOwners[_name] = _address;
         
     }
 }
